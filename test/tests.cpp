f2eb59f0dbf42caafd14d1ac083ee96881f0b153
David Poulton <david.kyle.poulton@cern.ch>
2022-07-20 17:55:50 +0200

    Modify CLI to be more testable, add tests
    Makes the arg parser function accept a vector of strings and overloads
    to add the generic argc, argv functionality. Also adds some initial
    tests for arg parsing.

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,6 +4,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 #include "ReadSpeed.hxx"
+#include "ReadSpeedCLI.hxx"
 
 #include "TFile.h"
 #include "TSystem.h"
@@ -27,6 +28,16 @@ void RequireFile(const std::string &fname, const std::vector<std::string> &branc
    for (int i = 0; i < 10000000; ++i)
       t.Fill();
    t.Write();
+}
+
+std::vector<std::string> ConcatVectors(const std::vector<std::string> &first, const std::vector<std::string> &second)
+{
+   std::vector<std::string> all;
+
+   all.insert(all.end(), first.begin(), first.end());
+   all.insert(all.end(), second.begin(), second.end());
+
+   return all;
 }
 
 TEST_CASE("Integration test")
@@ -94,4 +105,52 @@ TEST_CASE("Branch test")
    }
 
    gSystem->Unlink("test3.root");
+}
+
+TEST_CASE("CLI test")
+{
+   SUBCASE("Filename list")
+   {
+      const std::vector<std::string> baseArgs{"root-readspeed", "--trees", "t", "--branches", "x", "--files"};
+      const std::vector<std::string> inFiles{"file-a.root", "file-b.root", "file-c.root"};
+
+      const auto allArgs = ConcatVectors(baseArgs, inFiles);
+
+      const auto parsedArgs = ParseArgs(allArgs);
+      const auto outFiles = parsedArgs.fData.fFileNames;
+
+      CHECK_MESSAGE(outFiles.size() == inFiles.size(),
+                    "Number of parsed files does not match number of provided files.");
+      CHECK_MESSAGE(outFiles == inFiles, "List of parsed files does not match list of provided files.");
+   }
+   SUBCASE("Tree list")
+   {
+      const std::vector<std::string> baseArgs{"root-readspeed", "--files", "file.root", "--branches", "x", "--trees"};
+      const std::vector<std::string> inTrees{"t1", "t2", "tree3"};
+
+      const auto allArgs = ConcatVectors(baseArgs, inTrees);
+
+      const auto parsedArgs = ParseArgs(allArgs);
+      const auto outTrees = parsedArgs.fData.fTreeNames;
+
+      CHECK_MESSAGE(outTrees.size() == inTrees.size(),
+                    "Number of parsed trees does not match number of provided trees.");
+      CHECK_MESSAGE(outTrees == inTrees, "List of parsed trees does not match list of provided trees.");
+   }
+   SUBCASE("Branch list")
+   {
+      const std::vector<std::string> baseArgs{
+         "root-readspeed", "--files", "file.root", "--trees", "t", "--branches",
+      };
+      const std::vector<std::string> inBranches{"x", "x_branch", "long_branch_name"};
+
+      const auto allArgs = ConcatVectors(baseArgs, inBranches);
+
+      const auto parsedArgs = ParseArgs(allArgs);
+      const auto outBranches = parsedArgs.fData.fBranchNames;
+
+      CHECK_MESSAGE(outBranches.size() == inBranches.size(),
+                    "Number of parsed trees does not match number of provided trees.");
+      CHECK_MESSAGE(outBranches == inBranches, "List of parsed trees does not match list of provided trees.");
+   }
 }
